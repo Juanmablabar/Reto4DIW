8c98356506cefc6190db0f76d2dcdc6ae8750b50
Juanmablaba <juanmablabardaw@gmail.com>
2024-02-22 22:26:46 +0100

    first commit

---
+++ Reto4-DIW-Geo/static/js/app.js
@@ -0,0 +1,139 @@
+var map = L.map('map').fitWorld();
+var youAreHere;
+var addLocation = document.getElementById('addLocation');
+var hist = document.getElementById('history');
+var dialAddLocation = document.getElementById('dialAddLocation');
+var saveLocation = document.getElementById('saveLocation');
+var panel = document.getElementById('bottomPanelInfo');
+var dialHist = document.getElementById('dialHistory');
+var closeHist = document.getElementById('closeHistory');
+
+
+// Inicializa el mapa
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        map.locate({setView: true, watch: true, maxZoom: 18});
+        youAreHere = L.marker([lat, lon]).addTo(map);
+    });
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+} else {
+    alert('Geolocalización no es compatible con este navegador.');
+}
+// Sigue al usuario según la posición (EN TEORÍA)
+map.on('locationfound', function(e){
+    var latlng = e.latlng;
+    youAreHere.setLatLng(latlng);
+    updatePanel();
+})
+
+// Abre el modal para guardar ubicaciones.
+addLocation.addEventListener('click', function(){
+    dialAddLocation.showModal()
+});
+
+var marker;
+// Cierra el modal para guardar ubicaciones.
+saveLocation.addEventListener('click', function(){
+    // Comprueba si hay marcador y borra si lo hay antes de añadir otro.
+    if(marker){
+        saveDeletedMarkers(marker);
+        map.removeLayer(marker);
+        panel.innerHTML = '';
+    }
+    // Establece los datos del marcador
+    var locationName = document.getElementById('locationName').value;
+    var latlng = youAreHere.getLatLng();
+    marker = L.marker(latlng).addTo(map);
+    marker.bindPopup(locationName).openPopup();
+    marker.timestamp = new Date().getTime();
+
+    dialAddLocation.close();
+    updatePanel();
+})
+
+
+
+var template = document.querySelector('template');
+var tableBody = document.getElementById('tableBody');
+
+// Mostrar el historial de ubicaciones
+hist.addEventListener('click', function(){
+    var locations = localStorage.getItem('locations') || [];
+    if (locations){
+        var locationTable = JSON.parse(locations);
+        tableBody.innerHTML = "";
+        locationTable.forEach(location =>{
+            let row= template.content.cloneNode(true);
+            row.querySelector('.C-name').textContent = location.name;
+            row.querySelector('.C-coords').textContent = location.latlng;
+            row.querySelector('.C-time').textContent = location.time+' min';
+            tableBody.appendChild(row);
+        })
+    }else{
+        let row= template.content.cloneNode(true);
+            row.querySelector('.C-name').textContent = "-";
+            row.querySelector('.C-coords').textContent = "-";
+            row.querySelector('.C-time').textContent = '-';
+            tableBody.appendChild(row);
+    }
+    dialHist.showModal();
+})
+
+// Cerrar el historial
+closeHist.addEventListener('click', function(){
+    dialHist.close();
+})
+
+// Eliminar marcador y guardar marcador
+map.on('contextmenu', function(e){
+    if(marker && confirm('¿Borrar este marcador?')){
+        saveDeletedMarkers(marker);
+        map.removeLayer(marker);
+        panel.innerHTML='';
+    }
+})
+
+
+// Guardar marcadores
+function saveDeletedMarkers(marker){
+    var locations = JSON.parse(localStorage.getItem('locations')) || [];
+    locations.push({
+        name: marker.getPopup().getContent(),
+        latlng: '('+marker.getLatLng().lat+','+marker.getLatLng().lng+')',
+        time: calculateTime(marker.timestamp)
+    });
+    localStorage.setItem('locations', JSON.stringify(locations));
+}
+
+// Distancia, temporizador y nombre
+function updatePanel(){
+    if(youAreHere){
+        var currentCoords = youAreHere.getLatLng();
+        var distance = calculateDistance(currentCoords, marker.getLatLng());
+        var time = calculateTime(marker.timestamp);
+    
+        panel.innerHTML = `
+        <p>Nombre: ${marker.getPopup().getContent()}</p>
+        <p>Distancia: ${distance.toFixed(2)} metros</p>
+        <p>Tiempo: ${time} minutos</p>`;
+    }else{
+        panel.innerHTML = "Ubicación desconocida";
+    }
+    
+}
+
+// Calcular distancia
+function calculateDistance (latlng1, latlng2){
+    return latlng1.distanceTo(latlng2);
+}
+
+// Calcular tiempo
+function calculateTime(markerTimeStamp){
+    var now = new Date().getTime();
+    var elapsedTime = now - markerTimeStamp;
+    return Math.floor(elapsedTime / (1000 *60)); //minutos
+}
---
+++ Reto4-DIW-Simon/static/js/app.js
@@ -0,0 +1,203 @@
+
+//VARIABLES DE ELEMENTOS HTML
+var start = document.getElementById("start");
+var mode = document.getElementById("mode");
+var saveMode = document.getElementById("saveMode");
+var difficulty = document.getElementById("difficulty");
+var buttons = document.querySelectorAll('.button');
+var score = document.getElementById('score');
+var rankingButton = document.getElementById('rankingButton');
+var scoreRanking = document.getElementById('scoreRanking');
+var ranking = document.getElementById('ranking');
+var hideRanking = document.getElementById('hideRanking');
+var template = document.querySelector('template');
+var sounds = {
+  green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+  red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+  blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+  yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
+}
+
+//VARIABLES INTERNAS JS
+var difficultyValue;
+var colors = ['red', 'green', 'blue', 'yellow'];
+var time;
+var playerSequence = [];
+var sequence = [];
+var isPlayerTurn = false;
+//VARIABLES TABLA PUNTUACIONES
+var playername;
+var playerRanking=[];
+
+
+rankingButton.addEventListener('click', function(){
+  showTable();
+  scoreRanking.showModal();
+})
+
+hideRanking.addEventListener('click', function(){
+  scoreRanking.close();
+})
+
+start.addEventListener('click', function () {
+  mode.showModal();
+})
+
+saveMode.addEventListener('click', function (e) {
+  difficultyValue = difficulty.value;
+
+  if (difficultyValue == "easy") {
+    time = 900;
+  } else if (difficultyValue == "medium") {
+    time = 600
+  } else if (difficultyValue == "hard") {
+    time = 400;
+  }
+  mode.close();
+  startGame();
+})
+
+function startGame() {
+  generateSequence();
+  playSequence();
+}
+
+// Función para generar una nueva secuencia
+function generateSequence() {
+  do {
+    var randomIndex = Math.floor(Math.random() * colors.length);
+
+  } while (randomIndex === sequence[sequence.length - 1] && randomIndex === sequence[sequence.length - 2]);
+  sequence.push(colors[randomIndex]);
+
+  for (i = 0; i < sequence.length; i++) {
+    console.log(sequence[i]);
+  }
+}
+
+
+//Funcion para los sonidos
+function playSound(color) {
+  var thisSound = color;
+  sounds[thisSound].currentTime=0;
+  sounds[thisSound].play();
+};
+
+
+// Función para reproducir la secuencia
+function playSequence() {
+  isPlayerTurn = false;
+  var index = 0;
+  var interval = setInterval(function () {
+    if (index >= sequence.length) {
+      clearInterval(interval);
+      isPlayerTurn = true;
+      return;
+    }
+    var color = sequence[index];
+    flashButton(color);
+    playSound(color);
+    index++;
+  }, time);
+}
+
+// Función para iluminar un botón
+function flashButton(color) {
+  var button = document.querySelector('.' + color);
+  var activeClass = color + 'On';
+  button.classList.add(activeClass);
+
+  setTimeout(function () {
+    button.classList.remove(activeClass);
+  }, time / 2);
+}
+
+// Función para gestionar la entrada del jugador
+function playerInput(color) {
+  var i = 0;
+  console.log("te toca")
+  if (!isPlayerTurn) return;
+  playerSequence.push(color);
+  flashButton(color);
+  playSound(color);
+  checkSequence();
+}
+
+// Función para verificar la secuencia del jugador
+function checkSequence() {
+  if (playerSequence.length < sequence.length) {
+    if (playerSequence[playerSequence.length - 1] != sequence[playerSequence.length - 1]) {
+      alert('¡Perdiste! Intenta de nuevo.');
+      sequence = [];
+      loseGame();
+      return;
+    } else {
+      switch (difficultyValue) {
+        case "easy": score.innerText++;
+          break;
+        case "medium": score.innerText += 2;
+          break;
+        case "hard": score.innerText += 3;
+      }
+
+    }
+  } else {
+    if (playerSequence[playerSequence.length - 1] != sequence[playerSequence.length - 1]) {
+      alert('¡Perdiste! Intenta de nuevo.');
+      sequence = [];
+      loseGame();
+      //showTable()
+      return;
+    } else {
+      score.innerText++;
+      playerSequence = [];
+      startGame();
+    }
+  }
+}
+
+function showTable(){
+  let playerRankingJSON= localStorage.getItem("playerRanking");
+  if(playerRankingJSON){
+    let tableRanking = JSON.parse(playerRankingJSON);
+    ranking.innerHTML = "";
+    tableRanking.forEach(player => {
+      let row = template.content.cloneNode(true);
+      row.querySelector('.nameC').textContent = player.name;
+      row.querySelector('.scoreC').textContent = player.score;
+      row.querySelector('.difficultyC').textContent = player.difficulty;
+      ranking.appendChild(row);
+    });
+
+  }else{
+    let row = template.cloneNode(true);
+    row.querySelector('.nameC').textContent = 'N/A';
+      row.querySelector('.scoreC').textContent = 'N/A';
+      row.querySelector('.difficultyC').textContent = 'N/A';
+      ranking.appendChild(row);
+  }
+}
+// Función de terminar el juego
+function loseGame(){
+  playername=(prompt("Introduce tu nombre:").slice(0, 3));
+  var difficultyValueTranslated;
+  switch(difficultyValue){
+    case 'easy': difficultyValueTranslated='Fácil';
+    break;
+    case 'medium': difficultyValueTranslated="Media";
+    break;
+    case 'hard': difficultyValueTranslated='Difícil';
+  }
+  let playerScore = {
+    name: playername,
+    score: score.innerText,
+    difficulty: difficultyValueTranslated
+  };
+  let playerRankingJSON= localStorage.getItem("playerRanking");
+  let playerRanking = playerRankingJSON ? JSON.parse(playerRankingJSON) : [];
+  playerRanking.push(playerScore);
+  playerRanking.sort(function(a,b){
+    return b.score-a.score;
+  });
+  localStorage.setItem("playerRanking", JSON.stringify(playerRanking));
+}
